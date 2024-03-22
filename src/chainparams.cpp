36bd6f32a10e1fbad52a782c03330157e48b4831
1NF1N18Y <1nf1n19y@gmail.com>
2024-03-22 14:42:10 +0100

    revert to before node rewards

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -187,7 +187,68 @@ public:
 		
 		uint32_t nGenesisTime = 1701429301;	
 
- 
+        // This is used inorder to mine the genesis block. Once found, we can use the nonce and block hash found to create a valid genesis block
+		/////////////////////////////////////////////////////////////////
+		
+        // arith_uint256 test;
+        // bool fNegative;    
+        // bool fOverflow;
+        // test.SetCompact(0x1e00ffff, &fNegative, &fOverflow);
+        // std::cout << "Test threshold: " << test.GetHex() << "\n\n";
+
+        // int genesisNonce = 0;
+        // uint256 TempHashHolding = uint256S("0x0000000000000000000000000000000000000000000000000000000000000000");
+        // uint256 BestBlockHash = uint256S("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        // for (int i=0;i<40000000;i++) {
+        //     genesis = CreateGenesisBlock(nGenesisTime, i, 0x1e00ffff, 4, 5000 * COIN);
+        //     genesis.hashPrevBlock = TempHashHolding;
+		// 	// Depending on when the timestamp is on the genesis block. You will need to use GetX16RHash or GetX16RV2Hash. Replace GetHash() with these below
+        //     consensus.hashGenesisBlock = genesis.GetX16RHash();
+
+        //     arith_uint256 BestBlockHashArith = UintToArith256(BestBlockHash);
+        //     if (UintToArith256(consensus.hashGenesisBlock) < BestBlockHashArith) {
+        //         BestBlockHash = consensus.hashGenesisBlock;
+        //         std::cout << "CHOOSE: consensus.hashGenesisBlock.GetHex(): " << consensus.hashGenesisBlock.GetHex() << " Nonce: " << i << " nGenesisTime: " << nGenesisTime << "\n";
+        //         std::cout << "CHOOSE: BestBlockHash.GetHex(): " << BestBlockHash.GetHex() << " Nonce: " << i << " nGenesisTime: " << nGenesisTime << "\n";
+        //         std::cout << "CHOOSE: genesis.GetX16RHash().GetHex(): " << genesis.GetX16RHash().GetHex() << " Nonce: " << i << " nGenesisTime: " << nGenesisTime << "\n";
+        //         std::cout << "CHOOSE: PrevBlockHash: " << genesis.hashPrevBlock.GetHex() << "\n";
+        //     }
+
+        //     //TempHashHolding = consensus.hashGenesisBlock;
+
+        //     if (BestBlockHashArith < test) {
+        //         genesisNonce = i - 1;
+        //         break;
+        //     }
+        //     std::cout << "	 consensus.hashGenesisBlock.GetHex(): " << consensus.hashGenesisBlock.GetHex() << " Nonce: " << i << "\n";
+        // }
+        // std::cout << "\n";
+        // std::cout << "\n";
+        // std::cout << "\n";
+
+        // std::cout << "BestBlockHash.GetHex() to 0x" << BestBlockHash.GetHex() << std::endl;
+        // std::cout << "genesis.GetX16RHash().GetHex() to 0x" << genesis.GetX16RHash().GetHex() << std::endl;
+        // std::cout << "Genesis Nonce to " << genesisNonce << std::endl;
+        // std::cout << "Genesis Merkle " << genesis.hashMerkleRoot.GetHex() << std::endl;
+
+        // std::cout << "\n";
+        // std::cout << "\n";
+        // int totalHits = 0;
+        // double totalTime = 0.0;
+
+        // for(int x = 0; x < 16; x++) {
+        //     totalHits += algoHashHits[x];
+        //     totalTime += algoHashTotal[x];
+        //     std::cout << "hash algo " << x << " hits " << algoHashHits[x] << " total " << algoHashTotal[x] << " avg " << algoHashTotal[x]/algoHashHits[x] << std::endl;
+        // }
+
+        // std::cout << "Totals: hash algo " <<  " hits " << totalHits << " total " << totalTime << " avg " << totalTime/totalHits << std::endl;
+
+        // genesis.hashPrevBlock = TempHashHolding;
+
+        // return;
+		
+		/////////////////////////////////////////////////////////////////
 
 		genesis = CreateGenesisBlock(nGenesisTime, 16349779, 0x1e00ffff, 4, 5000 * COIN);
         consensus.hashGenesisBlock = genesis.GetX16RHash();	
@@ -246,8 +307,7 @@ public:
         nAddNullQualifierTagBurnAmount = .1 * COIN;
 		
         // 2% of 5000 COIN to Donation
-	    nCommunityAutonomousAmount = 5;
-        // nNodesPercentage = 3;
+	    nCommunityAutonomousAmount = 2;
 
         // Burn Addresses
 	    strIssueAssetBurnAddress = "PissueAssetXXXXXXXXXXXXXXXXa1oUfD";
@@ -264,8 +324,7 @@ public:
         strGlobalBurnAddress = "PBurnXXXXXXXXXXXXXXXXXXXXXXUkdzqy";
 
         // Donation Address
-        strCommunityAutonomousAddress = "PMusU6LgjQ3Yp947M7mm5Nca7avLJgvxfL";
-        // strNodes = "PMusU6LgjQ3Yp947M7mm5Nca7avLJgvxfL";
+        strCommunityAutonomousAddress = "PK8d2WCRHkwWyyrtJ2fdbisRuxxjWWnjwR";
 
         // DGW Activation
         nDGWActivationBlock = 1;
